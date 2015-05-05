abd3e5f96bea59a9108ed36c23e2a20fbfd68b6c
fam38 <stevensm@127.0.0.1>
2015-05-05 10:40:24 +0200

    async

---
+++ FrameWorkEX/src/appliTemoin/AppliUserAsync.java
@@ -0,0 +1,21 @@
+package appliTemoin;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+
+public class AppliUserAsync {
+
+	public static void main(String[] args) throws RemoteException {
+		// TODO Auto-generated method stub
+		OurUserAsync user = new OurUserAsync();
+		user.importServer("152.77.82.231", 1099);
+		Scanner sc = new Scanner(System.in);
+		do{
+		System.out.println("Fichier à envoyer : ");
+		String name = sc.nextLine();
+		user.send(user.takeObject(name),name );
+		}while(true);
+	}
+
+}
